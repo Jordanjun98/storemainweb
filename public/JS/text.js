a2480db2d115521bc9be47b31eb96b4b0a0fd250
Jordan <jordan@MacBook-Pro-Jordan.local>
2018-05-17 13:27:27 +0800

    firstcommit

--- public/JS/text.js
+++ public/JS/text.js
@@ -1,48 +1,20 @@
-
-//Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
-
 //Get the Modal
-var modal = document.getElementById('id02');
+alert("Hello");
 
-function displayCart(){
+document.getElementById('myModal').style.display='block';
 
-}
+
 
 
 // Get the modal
-function getmodal(){
-    var modal = document.getElementById('myModal');
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
 
 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+//firebase
+var Test1 = document.getElementById("test1");
+var Test2 = document.getElementById("test2");
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+function test1(){
+  var firebase = firebase.database().ref();
+  //window.alert("OKAY");
+  firebaseRef.child("text").set("Some value");
 }
