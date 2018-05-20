8c9039fa67465b8e5eed2c9b5b8d5df1038276c1
Jordan <jordan@MacBook-Pro-Jordan.local>
2018-05-20 17:19:03 +0800

    text.js

--- public/JS/text.js
+++ public/JS/text.js
@@ -1,20 +1,47 @@
 //Get the Modal
-alert("Hello");
 
-document.getElementById('myModal').style.display='block';
+// Get the modal element
+var modal = document.getElementById('simpleModal');
+
+window.onload=function(){
+  //Get the modal button
+  var modalBtn = document.getElementById("modalBtn");
+  if (modalBtn){
+    //Listen for open click
+    modalBtn.addEventListener("click",openModal);
+  }else{
+    console.log("Error");
+  }
+
+  //Get close Button
+  var closebtn = document.getElementById('closebtn');
+  if (closebtn){
+    //Listen for close click
+    closebtn.addEventListener('click',closeModal);
+  }else{
+    console.log("error");
+  }
+}
+
+//Function to open modal
+function openModal(){
+  document.getElementById("simpleModal").style.display = 'block';
+}
+
+//Function to close modal
+function closeModal(){
+  document.getElementById("simpleModal").style.display = 'none';
+}
 
 
-
-
-// Get the modal
 
 
 //firebase
-var Test1 = document.getElementById("test1");
+/*var Test1 = document.getElementById("test1");
 var Test2 = document.getElementById("test2");
 
 function test1(){
   var firebase = firebase.database().ref();
   //window.alert("OKAY");
   firebaseRef.child("text").set("Some value");
-}
+}*/
